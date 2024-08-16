e4ea78bdda7b247840a04566eb00f21fb5e7827b
kalebE36 <kaleb.ellington03@gmail.com>
2024-08-16 15:23:27 -0400

    added filtering and Tornado API

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -47,6 +47,7 @@ dependencies {
     implementation(libs.material)
     implementation(libs.play.services.maps)
     implementation(libs.androidx.constraintlayout)
+    implementation(libs.firebase.inappmessaging)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
---
+++ app/src/main/java/com/example/map_tutorial/arcDataModel.kt
@@ -0,0 +1,25 @@
+package com.example.map_tutorial
+
+data class ArcGisResponse(
+    val features: List<Feature>
+)
+
+data class Feature(
+    val attributes: Attributes,
+)
+
+data class Attributes(
+    val OBJECTID: Int?,
+    val UTC_DATETIME: String?,
+    val F_SCALE: String?,  // Might be nullable or empty
+    val LOCATION: String?,
+    val COUNTY: String?,
+    val STATE: String?,
+    val LATITUDE: Double?,
+    val LONGITUDE: Double?,
+    val COMMENTS: String?
+)
+
+
+
+
